01ab25c5729440ba7f70091bfd4afd5602f4fe6d
TalhaShaikh150 <tssmart233@gmail.com>
2025-06-23 09:11:08 +0500

    .Updated Ui For Feed Page And Home Page

--- scripts/feed.js
+++ scripts/feed.js
@@ -13,9 +13,9 @@ const dummyProfileSrc = "assets/profile.png";
 
 const feedContainer = document.querySelector(".feed-container");
 
+const userDetails = await getUserDetailsFromDb(); // all users
 async function generateFeed() {
-  const userDetails = await getUserDetailsFromDb(); // all users
-  feedContainer.innerHTML = ""; 
+  feedContainer.innerHTML = "";
 
   for (let i = 0; i < userDetails.length; i++) {
     const user = userDetails[i];
@@ -42,28 +42,50 @@ async function generateFeed() {
 
     const html = `
       <div class="feed-post">
-        <div class="d-flex">
-          <div class="profile-border">
-            <img src="${userProfile}" class="profile" />
-          </div>
-          <div class="username">${fullName}</div>
-        </div>
-        <img src="${userPost}" alt="Post Image" />
-        <div class="icons">
-          <div class="gap">
+      <div class="d-flex">
+      <div class="profile-border">
+      <img src="${userProfile}" class="profile" />
+      </div>
+      <div class="username">${fullName}</div>
+      </div>
+      <div class="feed-img-container">
+      <img src="${userPost}" alt="Post Image" />
+      </div>
+      <div class="icons">
+      <div class="gap">
             <i class="fa-regular fa-heart"></i>
             <i class="fa-regular fa-message"></i>
             <i class="fa-solid fa-paper-plane"></i>
           </div>
           <div>
             <i class="fa-solid fa-bookmark"></i>
-          </div>
-        </div>
-      </div>
+            </div>
+            </div>
+            </div>
     `;
 
-    feedContainer.innerHTML += html;
+    // feedContainer.innerHTML += html;
+
+    feedContainer.innerHTML = html + feedContainer.innerHTML;
   }
 }
 
 generateFeed();
+
+// function likeAnimation() {
+//   const allFeedPost = document.querySelectorAll(".feed-post");
+//   const heart = document.querySelectorAll(".like");
+
+//   allFeedPost.forEach((post) => {
+//     post.addEventListener("dblclick", () => {});
+//     heart.forEach((h) => {
+//       h.classList.remove("hide");
+//     });
+//   });
+// }
+
+// likeAnimation();
+
+{
+  /* <i class="fa-solid fa-heart like hide"></i> */
+}
